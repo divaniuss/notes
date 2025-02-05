01e5943d5ce8e73124f5517d3cfd12b06c5e4397
Dmitriy <dimasikemelka@gmail.com>
2025-02-05 11:28:15 +0200

    added class Manager

--- main.py
+++ main.py
@@ -15,5 +15,28 @@ class Note:
         return Note(title, text)
 
 
+class Manager:
+    def __init__(self):
+        self.notes = []
+
+    def get_all_notes(self):
+        return self.notes
+
+    def find_note(self, title: str):
+        return [note for note in self.notes if note.title == title]
+
+    def delete_note(self, title: str):
+        self.notes = [note for note in self.notes if note.title != title]
+
+manager = Manager()
+
 note1 = Note.create("Tema", "some text")
-print(note1)
+note2 = Note.create("Tema2", "some text2")
+
+
+manager.notes.append(note1)
+manager.notes.append(note2)
+
+for note in manager.get_all_notes():
+    print(note)
+    print('\n')
