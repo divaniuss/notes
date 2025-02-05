1b40cbbae757a4fed0d8bba5edc8b45c0c3c71b7
Dmitriy <dimasikemelka@gmail.com>
2025-02-05 11:18:03 +0200

    added class Note

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from datetime import datetime
 
+
+class Note:
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        return f"{self.title} ({self.created_at})\n{self.text}"
+
+
+    def create(title, text):
+        return Note(title, text)
+
+
+note1 = Note.create("Tema", "some text")
+print(note1)
